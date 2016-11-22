7640d7c3a0ec608b0f20f1dba616a27cc42abd07
madarou <2016333261@qq.com>
2016-11-22 22:18:18 +0800

    finish depart services

---
+++ src/main/java/com/makao/facade/impl/SeckillFacadeImpl.java
@@ -0,0 +1,56 @@
+package com.makao.facade.impl;
+
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.makao.dto.Exposer;
+import com.makao.dto.SeckillExecution;
+import com.makao.entity.Seckill;
+import com.makao.exception.RepeatKillException;
+import com.makao.exception.SeckillCloseException;
+import com.makao.exception.SeckillException;
+import com.makao.facade.SeckillFacade;
+import com.makao.service.SeckillService;
+
+/**
+ * @author ZYR
+ * dubbo接口实现
+ */
+@Service("seckillFacade")
+public class SeckillFacadeImpl implements SeckillFacade {
+	private Log LOG = LogFactory.getLog(this.getClass());
+
+    @Autowired
+    private SeckillService seckillService;
+    
+	@Override
+	public List<Seckill> getSeckillList() {
+		return seckillService.getSeckillList();
+	}
+
+	@Override
+	public Seckill getById(long seckillId) {
+		return seckillService.getById(seckillId);
+	}
+
+	@Override
+	public Exposer exportSeckillUrl(long seckillId) {
+		return seckillService.exportSeckillUrl(seckillId);
+	}
+
+	@Override
+	public SeckillExecution executeSeckill(long seckillId, long userPhone, String md5)
+			throws SeckillException, RepeatKillException, SeckillCloseException {
+		return seckillService.executeSeckill(seckillId, userPhone, md5);
+	}
+
+	@Override
+	public SeckillExecution executeSeckillProcedure(long seckillId, long userPhone, String md5) {
+		return seckillService.executeSeckillProcedure(seckillId, userPhone, md5);
+	}
+
+}
---
+++ src/main/java/com/makao/service/SeckillService.java
@@ -0,0 +1,67 @@
+package com.makao.service;
+
+import com.makao.dto.Exposer;
+import com.makao.dto.SeckillExecution;
+import com.makao.entity.Seckill;
+import com.makao.exception.RepeatKillException;
+import com.makao.exception.SeckillCloseException;
+import com.makao.exception.SeckillException;
+
+import java.util.List;
+
+/**
+ * Created by wchb7 on 16-5-13.
+ * 业务接口:站在"使用者"的角度设计接口
+ * 1.方法的定义的粒度.2.参数.3.返回类型(return /异常)
+ */
+public interface SeckillService {
+
+
+    /**
+     * 查询所有秒杀记录
+     *
+     * @return
+     */
+    List<Seckill> getSeckillList();
+
+
+    /**
+     * 查询单个秒杀记录
+     *
+     * @param seckillId
+     * @return
+     */
+    Seckill getById(long seckillId);
+
+
+    /**
+     * 秒杀开启时输出秒杀接口地址
+     * 否则输出系统时间和秒杀时间
+     *
+     * @param seckillId
+     * @return
+     */
+    Exposer exportSeckillUrl(long seckillId);
+
+    /**
+     * 执行秒杀操作
+     *
+     * @param seckillId
+     * @param userPhone
+     * @param md5
+     * @return
+     */
+    SeckillExecution executeSeckill(long seckillId, long userPhone, String md5) throws SeckillException
+            , RepeatKillException, SeckillCloseException;
+
+    /**
+     * 执行秒杀操作 by 存储过程
+     *
+     * @param seckillId
+     * @param userPhone
+     * @param md5
+     * @return
+     */
+    SeckillExecution executeSeckillProcedure(long seckillId, long userPhone, String md5);
+
+}
